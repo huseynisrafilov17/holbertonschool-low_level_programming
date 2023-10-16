0a066d239661785b6ea22ad4acfcf4c1c500e9f6
root <huseynisrafilov17@gmail.com>
2023-10-16 05:44:16 -0700

    no

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 122;
+
+	while (i > 97)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
