6351e5debfc329615b61d6a98dfbf6a3c2898688
root <huseynisrafilov17@gmail.com>
2023-10-16 05:20:32 -0700

    no

---
+++ variables_if_else_while/4-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i != 101 || i != 113)
+		{
+			putchar((char)i);
+			i++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
