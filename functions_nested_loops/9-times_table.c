992683f78b5a50276e400ea6851e8e4575be3bbd
root <huseynisrafilov17@gmail.com>
2023-10-17 07:51:36 -0700

    no

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,13 +12,17 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			a = i * j;
-			while (a / 10 != 0)
+			if (a / 10 != 0)
 			{
 				r = a % 10;
-				_putchar(r + '0');
 				a = a / 10;
+				_putchar(a + '0');
+				_putchar(r + '0');
 			}
-			_putchar(a + '0');
+			else
+			{
+				_putchar(a + '0');
+			}
 			if (j < 9)
 			{
 				_putchar(' ');
