cba7056d8630e493a6eaff43791dcad5fb40c886
root <huseynisrafilov17@gmail.com>
2023-10-17 07:52:41 -0700

    no

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -23,11 +23,11 @@ void times_table(void)
 			{
 				_putchar(a + '0');
 			}
-			if (j < 9)
+			if (j > 1 && j < 9)
 			{
-				_putchar(' ');
-				_putchar(' ');
 				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
