b77bc6bfb468b07b378ab6e8a77763bcc3d50d6a
root <huseynisrafilov17@gmail.com>
2023-10-18 02:48:38 -0700

    no

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,1 +1,25 @@
-
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long int a, b, s;
+	int c;
+	a = 1;
+	b = 2;
+	c = 3;
+	printf("%lu, %lu", a, b);
+	while (c < 99)
+	{
+		s = a + b;
+		a = b;
+		b = s;
+		c++;
+		printf(", %lu", s);
+	}
+	printf("\n");
+	return (0);
+}
