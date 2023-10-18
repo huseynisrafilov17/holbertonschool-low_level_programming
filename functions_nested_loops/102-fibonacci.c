05443895c094f41ca462c00174e52b31217e3f9f
root <huseynisrafilov17@gmail.com>
2023-10-18 02:41:58 -0700

    no

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long a, b, s;
+	int c;
+	a = 1;
+	b = 2;
+	c = 3;
+	printf("%ld, %ld", a, b);
+	while (c < 51)
+	{
+		s = a + b;
+		a = b;
+		b = s;
+		c++;
+		printf(", %ld", s);
+	}
+	printf("\n");
+	return (0);
+}
