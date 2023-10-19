04cd3c53e2ffaaab9324b1f5b438025f8cf82fe2
root <huseynisrafilov17@gmail.com>
2023-10-19 08:03:47 -0700

    no

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_diagonal -  draws a diagonal line in the terminal.
- * @n: int.
+ * print_square - prints a square.
+ * @size: int.
  * Return: void.
  */
 void print_square(int size)
@@ -10,7 +10,7 @@ void print_square(int size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j <= i; j++)
+		for (j = 0; j < size; j++)
 		{
 			_putchar('#');
 		}
