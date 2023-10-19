adb69e95fd31f31fc2f4d47ddea38f2aed12f6d8
root <huseynisrafilov17@gmail.com>
2023-10-19 08:10:53 -0700

    no

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d");
+		}
+		if (i < 100)
+		{
+			printf(" ");
+		}
+	}
+	return (0);
+}
