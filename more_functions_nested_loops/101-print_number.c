aaae0a398b3f074cb28cbf91636ee967ffc1a797
root <huseynisrafilov17@gmail.com>
2023-10-19 09:14:36 -0700

    no

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_number - prints an integer.
+ * @n: int.
+ * Return: void.
+ */
+
+void print_number(int n)
+{
+	unsigned int i = 10;
+	
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -1 * n;
+	}
+	while (n / i != 0) 
+	{
+		i = i * 10;
+	}
+	i = i / 10;
+	for (; i != 1; i /= 10)
+	{
+		_putchar((n / i) + '0');
+		n = n / i;
+	}
+	_putchar((n % 10) + '0');
+}
