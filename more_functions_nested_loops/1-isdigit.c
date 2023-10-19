f71dff5fc749632a671bf011043cb096abf6003a
root <huseynisrafilov17@gmail.com>
2023-10-19 07:09:05 -0700

    no

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+/**
+ *_isdigit - checks for a digit.
+ * @c: int.
+ * Return: 0 or 1.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
