8b9819826a068383b21e2aedac2bea58ed3a6751
root <huseynisrafilov17@gmail.com>
2023-10-19 09:26:23 -0700

    no

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -8,17 +8,20 @@
 void print_number(int n)
 {
 	unsigned int i = 10;
-	
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -1 * n;
 	}
-	while (n / i != 0) 
+	while (n / i != 0 || i < INT_MAX/10)
 	{
 		i = i * 10;
 	}
-	i = i / 10;
+	if (i < INT_MAX/10)
+		continue;
+	else
+		i = i / 10;
 	for (; i != 1; i /= 10)
 	{
 		_putchar((n / i) + '0');
