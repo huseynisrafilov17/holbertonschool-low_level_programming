56b7fd4594ea744298de245511b1409c8890d1fd
root <huseynisrafilov17@gmail.com>
2023-10-20 06:08:53 -0700

    no

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <inttypes.h>
 /**
  * main - Entry point
  *
@@ -6,20 +7,80 @@
  */
 int main(void)
 {
-	unsigned long int a, b, s;
+	uint64_t a, b, s;
+	uint64_t a_half1, b_half1, a_half2, b_half2, s_half1, s_half2, divider;
 	int c;
+	divider = 10000000000;
 	a = 1;
 	b = 2;
 	c = 3;
-	printf("%lu, %lu", a, b);
+	printf("%"PRIu64", %"PRIu64, a, b);
 	while (c < 99)
 	{
-		s = a + b;
-		a = b;
-		b = s;
-		c++;
-		printf(", %lu", s);
+		if (c < 93)
+		{
+			s = a + b;
+			a = b;
+			b = s;
+			c++;
+			printf(", %"PRIu64, s);
+		}
+		else if (c == 93)
+		{
+			a_half1 = a / 10000000000;
+			a_half2 = a % 10000000000;
+			b_half1 = b / 10000000000;
+			b_half2 = b % 10000000000;
+			s_half1 = a_half1 + b_half1;
+			s_half2 = a_half2 + b_half2;
+			if (s_half2 > 9999999999)
+			{
+				s_half1 = s_half1 + (s_half2 / 10000000000);
+				s_half2 = s_half2 - (s_half2 / 10000000000) * 10000000000;
+			}
+			a_half1 = b_half1;
+			a_half2 = b_half2;
+			b_half1 = s_half1;
+			b_half2 = s_half2;
+			printf (", %"PRIu64"%"PRIu64, s_half1, s_half2);
+			c++;
+		}
+		else if (c > 93 && c <= 97)
+		{
+			s_half1 = a_half1 + b_half1;
+			s_half2 = a_half2 + b_half2;
+			if (s_half2 > 9999999999)
+                        {
+				s_half1 = s_half1 + (s_half2 / 10000000000);
+				s_half2 = s_half2 - (s_half2 / 10000000000) * 10000000000;
+                        }
+			a_half1 = b_half1;
+			a_half2 = b_half2;
+			b_half1 = s_half1;
+			b_half2 = s_half2;
+			printf (", %"PRIu64"%"PRIu64, s_half1, s_half2);
+			c++;
+		}
+		else
+		{
+			s_half1 = a_half1 + b_half1;
+			s_half2 = a_half2 + b_half2;
+			if (s_half2 > 99999999999)
+			{
+				s_half1 = s_half1 + (s_half2 / 100000000000);
+				s_half2 = s_half2 - (s_half2 / 100000000000) * 100000000000;
+			}
+			a_half1 = b_half1;
+			a_half2 = b_half2;
+			b_half1 = a_half1;
+			b_half2 = a_half2;
+			printf (", %"PRIu64"%"PRIu64, s_half1, s_half2);
+			c++;
+		}
+
 	}
 	printf("\n");
 	return (0);
 }
+
+
