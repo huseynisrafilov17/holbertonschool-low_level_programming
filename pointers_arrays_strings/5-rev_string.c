23daf66ea9e60352e07e3656577f4b7cbbabf9da
root <huseynisrafilov17@gmail.com>
2023-10-23 05:44:42 -0700

    no

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -5,16 +5,20 @@
 void rev_string(char *s)
 {
 	int c = 0;
+	char *a;
 
 	while (*s != '\0')
 	{
+		*a = *s;
 		s++;
+		a++;
 		c++;
 	}
 	s--;
 	for (; c > 0; c--)
 	{
-		*s = *(s - c);
+
+		*s = *(a - c);
 		s--;
 	}
 }
