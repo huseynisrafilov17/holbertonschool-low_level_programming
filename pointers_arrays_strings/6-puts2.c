87b7a14650fe7a9d52bfaebfdb05e549516bb9d0
root <huseynisrafilov17@gmail.com>
2023-10-23 05:50:55 -0700

    no

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string.
+ * @str: char.
+ */
+void puts2(char *str)
+{
+	int c = 0;
+
+	while (*str != '\0')
+	{
+		if (c % 2 == 0)
+		{
+		_putchar(*str);
+		}
+		str++;
+		c++;
+	}
+}
