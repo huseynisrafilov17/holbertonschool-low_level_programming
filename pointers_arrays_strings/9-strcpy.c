3e068d25c08a8a2c5987d935a9e026049d6b375c
root <huseynisrafilov17@gmail.com>
2023-10-23 07:28:40 -0700

    no

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - copies string.
+ * @dest: destination char.
+ * @src: source string.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int c = 0;
+
+	while (*(src - 1) != "\0")
+	{
+		*(dest + c) = *src;
+		src++;
+	}
+	return (*dest);
+}
