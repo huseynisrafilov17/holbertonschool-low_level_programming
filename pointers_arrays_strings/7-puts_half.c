23e6470376676ba7b8072a395e07cc32d3c4c0d7
root <huseynisrafilov17@gmail.com>
2023-10-23 07:05:42 -0700

    no

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+void puts_half(char *str)
+{
+	int a;
+
+	a = strlen(*str);
+	if (a % 2 == 0)
+	{
+		a = a / 2;
+	}
+	else
+	{
+		a = (a + 1) / 2;
+	}
+	while (*s + a != '\0')
+	{
+		_putchar(*s + a);
+		s++;
+	}
+}
