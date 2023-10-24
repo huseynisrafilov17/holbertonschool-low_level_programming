c89f8850ada94d704b8cc1f7ace5d0389af84472
root <huseynisrafilov17@gmail.com>
2023-10-24 04:52:36 -0700

    no

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -6,17 +6,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	/*int c = 0;*/
+	int c = 0;
 
 	while (*dest != '\0')
 	{
 		dest++;
 	}
 	*dest = *src;
-	/*while (*(src + c - 1) != '\0')
+	while (*(src + c - 1) != '\0')
 	{
 		*(dest + c) = *(src + c);
 		c++;
-	}*/
+	}
 	return (dest);
 }
