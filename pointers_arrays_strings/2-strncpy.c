04ee3531a4583aa0bd002f8b73c774a14b1777a0
root <huseynisrafilov17@gmail.com>
2023-10-24 06:57:31 -0700

    no

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,14 @@
+/**
+ * _strncpy - copies a string.
+ * @dest: char.
+ * @src: char.
+ * @n: int.
+ * Return: string.
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j = 0;
-	
+
 	while (j < n && *(src + j) != '\0')
 	{
 		*(dest + j) = *(src + j);
