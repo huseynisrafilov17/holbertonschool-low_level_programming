bb7cac735ab6aa20bb7d13223c8cb64a0afd0839
root <huseynisrafilov17@gmail.com>
2023-10-24 09:08:15 -0700

    no

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -5,15 +5,23 @@
  */
 void reverse_array(int *a, int n)
 {
-	int temp, i, j;
-
+	int temp, i, j, half_n;
+	if (n % 2 == 0)
+	{
+		half_n = n / 2;
+	}
+	else
+	{
+		half_n = (n + 1) / 2;
+	}
 	i = n - 1;
 	j = 0;
-	while (i >= 0)
+	while (i >= half_n)
 	{
-		temp = *a;
+		temp = *(a + j);
 		*(a + j) = *(a + i);
+		*(a + i) = temp;
 		j++;
-		i--
+		i--;
 	}
 }
