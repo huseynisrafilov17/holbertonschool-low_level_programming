e23a5bbf02332a5cb3dec3d687e9a0f00f4d5570
root <huseynisrafilov17@gmail.com>
2023-10-24 04:47:50 -0700

    no

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings.
+ * @dest: destination.
+ * @src: source.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int c = 0;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*(src + c - 1) != '\0')
+	{
+		*(dest + c) = *(src + c);
+	}
+	return (dest);
+}
