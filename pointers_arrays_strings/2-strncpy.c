bd7a106ecfbad3f18b0ee7756d2143e9f365dfb8
root <huseynisrafilov17@gmail.com>
2023-10-24 07:45:22 -0700

    no

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,16 +11,16 @@ char *_strncpy(char *dest, char *src, int n)
 	int j;
 	j = 0;
 
-	while (j < n && *(src + j) != '\0')
+	while (j < n)
 	{
 		if (*(src + j) != '\0')
 		{
 			*(dest + j) = *(src + j);
-			j++
+			j++;
 		}
 		else
 		{
-			*(dest + j) = '\0'
+			*(dest + j) = '\0';
 			j++;
 		}
 	}
