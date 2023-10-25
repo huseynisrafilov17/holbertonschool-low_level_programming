950c506235f5169dce86b5fcf9e70af1e15d93dc
root <huseynisrafilov17@gmail.com>
2023-10-25 07:21:16 -0700

    no

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+/**
+ * rot13 - encodes a string using rot13.
+ * @zor: string.
+ * Return: string.
+ */
+char *rot13(char *zor)
+{
+	char c;
+	int i = 0;
+
+	while (*(zor + i) != '\0')
+	{
+		c = *(zor + i);
+		for (; (c >= 65 && c <= 90) || (c >= 97 && c <= 122);)
+		{
+			if ((c >= 65 && c <= 77) || (c >= 97 && c <= 109))
+			{
+				*(zor + i) = c + 13;
+			}
+			else
+			{
+				*(zor + i) = c - 13;
+			}
+		}
+	}
+	return (zor);
+}
