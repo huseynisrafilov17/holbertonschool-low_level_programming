3d9d4789a09fa0f27b6c8688646bcc5b9dc79932
root <huseynisrafilov17@gmail.com>
2023-10-26 07:31:28 -0700

    no

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -1,22 +1,24 @@
 #include <string.h>
 int add_part1 (char *n1, char *n2, int *s1, int *s2, char *r)
 {
-	int n_1, n_2, temp, i;
+	int n_1, n_2, temp, i, size1, size2;
+	size1 = *s1;
+	size2 = *s2;
 	temp = 0;
 	i = (*s1 > *s2) ? *s1 : *s2;
 	while ((*s1 >= *s2 && *s2 >= 0) || (*s2 > *s1 && *s1 >= 0))
 	{
-		n_1 = (int)*(n1 + s1) - 48;
-		n_2 = (int)*(n2 + s2) - 48;
+		n_1 = (int)*(n1 + size1) - 48;
+		n_2 = (int)*(n2 + size2) - 48;
 		*(r + i)= (n_1 + n_2 + temp) % 10 + 48;
 		temp = (n_1 + n_2 + temp) / 10;
-		*s1 = *s1 - 1;
-		*s2 = *s2 - 1;
+		size1--;
+		size2--;
 		i--;
 	}
-	for (; *s1 >= 0 || *s2 >= 0;)
+	for (; size1 >= 0 || size2 >= 0;)
 	{
-		if (*s1 >= 0)
+		if (size1 >= 0)
 		{
 			n_1 = (int)*(n1 + s1) - 48;
 			*(r + i)= (n_1 + temp) % 10 + 48;
@@ -24,12 +26,12 @@ int add_part1 (char *n1, char *n2, int *s1, int *s2, char *r)
 			*s1 = *s1 - 1;
 			i--;
 		}
-		else if (*s2 >= 0)
+		else if (size2 >= 0)
 		{
 			n_2 = (int)*(n2 + s2) - 48;
 			*(r + i)= (n_2 + temp) % 10 + 48;
 			temp = (n_2 + temp) / 10;
-			*s2 = *s2 - 1;
+			size2 = size2 - 1;
 			i--;
 		}
 	}
