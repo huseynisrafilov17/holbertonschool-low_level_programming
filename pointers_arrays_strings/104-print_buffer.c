5f8a7a85200f56b6d4226604553c1a3804e9dc96
root <huseynisrafilov17@gmail.com>
2023-10-27 04:00:21 -0700

    no

---
+++ pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void print_buffer(char *b, int size)
+{
+	int i = 0;
+
+	while (i <= size)
+	{
+		if (i % 10 == 0)
+		{
+			if (*b < 32)
+				printf("%p: 0%x", b, *b);
+			else
+				printf("%p: %x", b, *b);
+		}
+		else if (i % 2 == 1)
+		{
+			if (*b < 32)
+				printf("0%x ", *b);
+			else
+				printf("%x ", *b);
+		}
+		else
+		{
+			if (*b < 32)
+				printf("0%x ", *b);
+			else
+				printf("%x ", *b);
+		}
+	}
+}
