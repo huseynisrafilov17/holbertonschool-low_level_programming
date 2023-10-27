5d4587dcc228e5895c8cf0b80c7882bfc21542e2
root <huseynisrafilov17@gmail.com>
2023-10-27 07:05:43 -0700

    no

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,3 +1,10 @@
+/**
+ * _memcpy - copies memory area.
+ * @dest: destination.
+ * @src: source.
+ * @n: times.
+ * Return: dest pointer.
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
