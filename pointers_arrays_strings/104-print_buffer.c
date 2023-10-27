bdee89dce27e48fe9ee28c3af276190859311ac6
root <huseynisrafilov17@gmail.com>
2023-10-27 04:41:42 -0700

    no

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -2,14 +2,12 @@
 void print_buffer(char *b, int size)
 {
 	int i = 0;
-	char *add;
-	long int diff;
 
 	while (i <= size)
 	{
 		if (i % 10 == 0)
 		{
-			printf("%08x: %02x", &(*(b + i)) - b, *(b + i));
+			printf("%08lx: %02x", &(*(b + i)) - b, *(b + i));
 		}
 		else if (i % 2 == 1)
 		{
