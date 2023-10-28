c2cd5396aa8859f4d13c39e6a7760c1b008fa7e6
root <huseynisrafilov17@gmail.com>
2023-10-28 01:57:26 -0700

    no

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -4,10 +4,15 @@
  * @b: string
  * @size: size of b.
  */
-/**void add_spaces (int i, int j)
+void add_spaces (int i)
 {
-	continue;
-}*/
+	int n_spaces = 25 - ((i + 1) % 10) * 5 / 2;
+	while (n_spaces > 0)
+	{
+		printf(" ");
+		n_spaces--;
+	}	
+}
 void print_buffer(char *b, int size)
 {
 	int i = 0, j = 0;
@@ -24,11 +29,12 @@ void print_buffer(char *b, int size)
 		{
 			for (; j <= i; j++)
 			{
+				if (i == size - 1)
+				{
+					add_spaces(i);
+				}
 				if (*(b + j) >= 32 && *(b + j) <= 126 && j % 10 == 0)
 				{
-					if (i == size - 1)
-						printf("%19c", *(b + j));
-					else
 						printf("%c", *(b + j));
 				}
 				else if (*(b + j) >= 32 && *(b + j) <= 126 && j % 10 == 9)
