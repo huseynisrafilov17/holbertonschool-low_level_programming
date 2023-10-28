ea45dac1f92fd5b79e5843e82aaa37ccbc9f13b5
root <huseynisrafilov17@gmail.com>
2023-10-28 08:52:40 -0700

    no

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,8 +9,8 @@ void print_diagsums(int *a, int size)
 	int i, sum1, sum2;
 	for (i = 0; i < size; i++)
 	{
-		sum1 += (*(a + i))[i];
-		sum2 += (*(a + size - i -1))[i];
+		sum1 += *(a + (size + 1) * i);
+		sum2 += *(a + (size - 1) * (i + 1));
 	}
 	printf("%d, %d", sum1, sum2);
 }
