f7a42d66286d49c65490d690aa1bdb05a0b5a68c
root <huseynisrafilov17@gmail.com>
2023-10-29 01:23:54 -0700

    no

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,11 +1,36 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-char* main(void)
+
+/**
+ * main - Generate a random password for 101-crackme file
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
 {
-	int a;
-	srand((int)time(NULL));
-	a = rand();
-	printf("ZaX%d$!AzX", a);
-	return ("Geek$ForGeeks7");
+	int password[40], i, sum = 0, n;
+
+	srand(time(NULL));
+	for (i = 0; i < 40; i++)
+	{
+		password[i] = rand() % 94 + 33;
+		sum+= password[i];
+		putchar(password[i]);
+
+		if (2772 - sum <= 126 && 2772 - sum > 32)
+		{
+			n = 2772 - sum;
+			sum += n;
+			putchar(n);
+			break;
+		}
+		else if (2772 - sum <= 32)
+		{
+			sum = 0;
+			i = -1;
+		}
+	}
+	return (0);
 }
