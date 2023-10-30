3758f077ae3ef2917e0ac0d63c373613f231fa6b
root <huseynisrafilov17@gmail.com>
2023-10-30 05:19:47 -0700

    no

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -5,10 +5,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		++s;
-		_print_rev_recursion(s);
+		return;
 	}
-	_putchar(*(s - 1));
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
