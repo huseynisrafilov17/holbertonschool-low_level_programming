41fafad8b56305854ea6fa5a0cb260b3581ffeee
root <huseynisrafilov17@gmail.com>
2023-10-30 05:50:31 -0700

    no

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,3 +1,9 @@
+/**
+ * _pow_recursion - returns the value of x raised to the power of y.
+ * @x: integer.
+ * @y: power.
+ * Return: the value of x raised to the power of y.
+ */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
