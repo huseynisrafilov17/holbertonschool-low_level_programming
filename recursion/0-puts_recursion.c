b189a8040ee99bbfc7c53850e099960942a4b7c8
root <huseynisrafilov17@gmail.com>
2023-10-30 04:39:38 -0700

    no

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _puts_recursion -  prints a string, followed by a new line.
+ * @s: string.
  */
 void _puts_recursion(char *s)
 {
@@ -8,6 +9,4 @@ void _puts_recursion(char *s)
 	s++;
 	if (*s != '\0')
 		_puts_recursion(s);
-	else
-		_putchar('\n');
 }
