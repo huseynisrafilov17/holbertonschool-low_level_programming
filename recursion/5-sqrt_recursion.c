8d04123610f1ccaefd49d296dc1a1ae2f4f69a0a
root <huseynisrafilov17@gmail.com>
2023-10-30 21:19:25 -0700

    no

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,15 +1,22 @@
-int _sqrt (int root, int n)
+/**
+ * _sqrt - finds root.
+ * @root: root of n.
+ * @n: number.
+ * Return: root.
+ */
+int _sqrt(int root, int n)
 {
 	if (root * root < n)
-        {
-                return (_sqrt(root + 1, n));
-        }
+		return (_sqrt(root + 1, n));
 	else if (root * root > n)
-        {
-                return (-1);
-        }
+		return (-1);
 	return (root);
 }
+/**
+ * _sqrt_recursion - a function that returns the natural square root of a number.
+ * @n: number.
+ * Return: passes control to other function.
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
