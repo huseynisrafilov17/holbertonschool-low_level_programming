e9ec3e1946d3544bc4ccb545a3b8d250dec24e8b
root <huseynisrafilov17@gmail.com>
2023-11-01 04:58:20 -0700

    no

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,13 +1,25 @@
 #include <string.h>
+/**
+ * palindrome_check - returns 1 if a string is a palindrome and 0 if not.
+ * @i: integer.
+ * @s: string.
+ * Return: 0 or 1.
+ */
 int palindrome_check(int i, char *s)
 {
 	int len = strlen(s);
-	if (*(s + i) == *(s + len - i - 1) && i != (len + 1) / 2 )
+
+	if(*(s + i) == *(s + len - i - 1) && i != (len + 1) / 2 )
 		return (palindrome_check(i + 1, s));
-	else if (*(s + i) == *(s + len - i - 1) && i == (len + 1) / 2 )
+	else if(*(s + i) == *(s + len - i - 1) && i == (len + 1) / 2 )
 		return (1);
 	return (0);
 }
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ * @s: string.
+ * Return: 0 or 1.
+ */
 int is_palindrome(char *s)
 {
 	return (palindrome_check(0, s));
