dc5b1e8b89ee3a8a3cefef5463337a374490e97f
root <huseynisrafilov17@gmail.com>
2023-11-01 04:52:59 -0700

    no

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -4,10 +4,9 @@ int palindrome_check(int i, char *s)
 	int len = strlen(s);
 	if (*(s + i) == *(s - len - i - 1) && i != (len + 1) / 2 )
 		return (palindrome_check(i + 1, s));
-	else if (*(s + i) != *(s - len - i - 1))
-		return (0);
-	if (*(s + i) == *(s - len - i - 1) && i == (len + 1) / 2 )
+	else if (*(s + i) == *(s - len - i - 1) && i == (len + 1) / 2 )
 		return (1);
+	return (0);
 }
 int is_palindrome(char *s)
 {
