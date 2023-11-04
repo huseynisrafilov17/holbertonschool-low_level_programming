b1f117023d1a08f7fa63977c3e612745064ae866
root <huseynisrafilov17@gmail.com>
2023-11-04 05:29:37 -0700

    no

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -12,6 +12,8 @@ int wildcmp(char *s1, char *s2)
 		if (*(s2 + 1) == '*')
 			return (wildcmp(s1, s2 + 1));
 		else if (*(s1 + 1) == *(s2 + 1))
+			if (strlen(s1) != strlen(s2))
+				return (wildcmp(s1 + 1, s2));
 			return (wildcmp(s1 + 1, s2 + 1));
 		return (wildcmp(s1 + 1, s2));
 	}
