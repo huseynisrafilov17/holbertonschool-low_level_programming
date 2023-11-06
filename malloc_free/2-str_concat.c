20dd8997f6fe3453ee9b0006baa17047bce0343d
root <huseynisrafilov17@gmail.com>
2023-11-06 05:34:53 -0800

    no

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,11 +9,15 @@ char *str_concat(char *s1, char *s2)
 	int i, s1_len, s2_len;
 
 	if (s1 == NULL)
-		*s1 = "";
-	s1_len = strlen(s1);
+		s1_len = 0;
+	else
+		s1_len = strlen(s1);
 	if (s2 == NULL)
-		*s2 = "";
-	s2_len = strlen(s2);
+		s2_len = 0;
+	else
+		s2_len = strlen(s2);
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
 	new = malloc(s1_len + s2_len + 1);
 	if (new == NULL)
 		return (NULL);
