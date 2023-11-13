2a1a1c5a2ad2f2cb5d000de895a4041ca37ffafb
root <huseynisrafilov17@gmail.com>
2023-11-13 05:34:28 -0800

    no

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i;
+	char *s;
+
+	if (n > (int)strlen(s2))
+		n = (int)strlen(s2);
+	s = malloc(((int)strlen(s1) + n + 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < (int)strlen(s1) + n; i++)
+	{
+		if (i < (int)strlen(s1))
+			s[i] = s1[i];
+		else
+			s[i] = s2[i];
+	}
+	return (s);
+}
