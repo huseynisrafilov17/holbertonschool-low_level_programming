e8dcc6ae0879179d9133a5cf3ab60d85663c4150
root <huseynisrafilov17@gmail.com>
2023-11-13 04:17:50 -0800

    no

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,12 @@
 #include <stdlib.h>
+/**
+ * malloc_checked - checks malloc and allocates.
+ * @b: size.
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *a;
+
 	a = malloc(b);
 	if (a == NULL)
 		exit(98);
