ce9f41071e9ead99f616909068a43597e92f518c
root <huseynisrafilov17@gmail.com>
2023-11-14 04:22:35 -0800

    no

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+int *array_range(int min, int max)
+{
+	int *arr, i;
+
+	if (min > max)
+		return (NULL);
+	*arr = malloc((max - min + 1) * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
+	for (i = min; i <= max; i++)
+		arr[i-min] = i;
+	return (arr);
+}
