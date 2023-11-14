a2f273b017c6e0ff47be5236f4ab03bf37e4f316
root <huseynisrafilov17@gmail.com>
2023-11-14 05:09:58 -0800

    no

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -8,8 +8,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	s = malloc(nmemb * size);
-	if (s == NULL)
-		return (NULL);
-	*s = NULL;
+	bzero(s, nmemb*size);
 	return (s);
 }
