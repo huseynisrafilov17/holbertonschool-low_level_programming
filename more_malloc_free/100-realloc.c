30641f067c31fb4c4868f34f301385b2e5173e2a
root <huseynisrafilov17@gmail.com>
2023-11-14 05:41:16 -0800

    no

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -7,7 +7,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	new_ptr = malloc(new_size);
-	if (new_ptr == NULL || new_size == 0)
+	if (new_ptr == NULL)
 	{
 		free(ptr);
 		return (NULL);
