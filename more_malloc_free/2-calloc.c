2971259e176e9230ad4fcbd6a48a463b4824647a
root <huseynisrafilov17@gmail.com>
2023-11-14 05:13:30 -0800

    no

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <string.h>
-
+/**
+ * _calloc - calloc.
+ * @nmemb: member number.
+ * @size: size of each member.
+ * Return: pointer.
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *s;
@@ -8,6 +13,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	s = malloc(nmemb * size);
-	bzero(s, nmemb*size);
+	if (s == NULL)
+		return (NULL);
+	bzero(s, nmemb * size);
 	return (s);
 }
