92c5a290a94c41dca6042dfe736732e6b21b328a
root <huseynisrafilov17@gmail.com>
2023-11-19 04:54:01 -0800

    no

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -12,22 +12,26 @@ int isint(char *s)
 }
 void multiplication(char *s1, char *s2, char *result,int length)
 {
-	char *another_ptr = result + length;
+	char *another_ptr = result + length, *inside_ptr;
 	int i, j, l1 = strlen(s1) - 1, l2 = strlen(s2) - 1;
 	int carry = 0, a;
 
 	for (i = l1; i >= 0; i--)
+	{
+		inside_ptr = another_ptr;
 		for (j = l2; j >=0; j--)
 		{
-			if (*another_ptr < 48 || *another_ptr > 57)
-				*another_ptr = '0';
-			a = carry + ((int)s1[i] - 48)* ((int)s2[j] - 48) + (int)*another_ptr - 48;
-			*another_ptr = a % 10 + 48;
+			if (*inside_ptr < 48 || *inside_ptr > 57)
+				*inside_ptr = '0';
+			a = carry + ((int)s1[i] - 48)* ((int)s2[j] - 48) + (int)*inside_ptr - 48;
+			*inside_ptr = a % 10 + 48;
 			carry = a / 10;
-			another_ptr--;
+			inside_ptr--;
 		}
+		another_ptr--;
+	}
 	if (carry != 0)
-		*(another_ptr - 1) = carry + 48;
+		*(inside_ptr - 1) = carry + 48;
 	while (result[0] < 48 || result[0] > 57)
 		for (i = 0; i <= length; i++)
 			result[i] = result[i + 1];
@@ -52,6 +56,7 @@ int main(int argc, char *argv[])
 		printf("Failed");
 		return (1);
 	}
+	s[strlen(argv[1]) + strlen(argv[2])] = '\0';
 	multiplication(argv[1], argv[2], s, strlen(argv[1]) + strlen(argv[2]) - 1);
 	printf("%s\n", s);
 	free(s);
