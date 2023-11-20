ff9e63e4892e53a0a58b9975397dc4167498b8c6
root <huseynisrafilov17@gmail.com>
2023-11-20 10:25:41 -0800

    no

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,9 +13,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
+	{
+		free(new_dog);
 		return (NULL);
-	strcpy(new_dog->name, name);
+	}
+	new_dog->name = name;
 	new_dog->age = age;
-	strcpy(new_dog->owner, owner);
+	new_dog->owner = owner;
 	return (new_dog);
 }
