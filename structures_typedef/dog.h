a2aa3c74ded7d14964312c375e49d50784210e6c
root <huseynisrafilov17@gmail.com>
2023-11-20 03:44:12 -0800

    no

---
+++ structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - dog structure.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
