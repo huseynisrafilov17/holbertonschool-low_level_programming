ea23941ebb514c65589e49b8492a8ceb189a8f64
root <huseynisrafilov17@gmail.com>
2023-11-20 04:00:23 -0800

    no

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,12 @@
 #include "dog.h"
 #include <stdlib.h>
+/**
+ * init_dog: initializes dog.
+ * @d: dog.
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner of dog.
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
