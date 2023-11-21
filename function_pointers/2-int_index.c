83f2e886eeff52eb83e005f418149c222735d85d
root <huseynisrafilov17@gmail.com>
2023-11-21 05:09:18 -0800

    no

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -8,13 +8,12 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
-	if (size <= 0)
-		return (-1);
-	if (array != NULL && cmp != NULL)
+
+	if (array != NULL && cmp != NULL && size <= 0)
 	{
 		for (; i < size; i++)
 			if ((*cmp)(array[i]))
 				return (i);
-		return (-1);
 	}
+	return (-1)
 }
