64d7f28c630a748326c6f1470102c7c0035eca7b
root <huseynisrafilov17@gmail.com>
2023-11-22 04:02:37 -0800

    no

---
+++ function_pointers/3-main.c
@@ -0,0 +1,17 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <string.h>
+
+int main(int argv, char *argc[])
+{
+	int a;
+
+	if (argv != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = get_op_func(argc[2])(atoi(argc[1]), atoi(argc[3]));
+	printf("%d\n", a);
+	return (0);
+}
