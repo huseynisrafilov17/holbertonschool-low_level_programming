5103096dbd781bb2ab4cdda8052d099b63cd9c7c
root <huseynisrafilov17@gmail.com>
2023-11-23 06:32:23 -0800

    no

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,35 @@
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	int i = 0;
+
+	va_start(ap, format);
+	while (*(format + i) != '\0' && (*(format + i) == 's' || *(format + i) == 'c'))
+	{
+		if (*format == 's')
+		{
+			printf("%s", va_arg(ap, char *));
+			i++;
+			continue;
+		}
+		printf("%c", va_arg(ap, char));
+		i++;
+	}
+	i = 0;
+	while (*(format + i) != '\0' && (*(format + i) == 'i' || *(format + i) == 'f'))
+	{
+		if (*format == 'i')
+		{
+			printf("%d", va_arg(ap, int));
+			i++;
+			continue;
+		}
+		printf("%f", va_arg(ap, float));
+		i++;
+	}
+	va_end(ap);
+}
