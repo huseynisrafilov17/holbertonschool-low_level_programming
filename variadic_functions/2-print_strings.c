f8f51aaf44c95b3b63d445c318787a3a6c91e36b
root <huseynisrafilov17@gmail.com>
2023-11-23 05:52:46 -0800

    no

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_strings - prints arguments in strings.
+ * @separator: separator.
+ * @n: count of strings.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		if (va_arg(ap, char *) != NULL)
+			printf("%s", va_arg(ap, char *));
+		else if (va_arg(ap, char *) == NULL)
+			printf("(nil)");
+		if (separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
