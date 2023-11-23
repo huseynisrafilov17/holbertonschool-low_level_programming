30403ef6bfff77a46fb26c7a01865c50bfca3456
root <huseynisrafilov17@gmail.com>
2023-11-23 05:35:24 -0800

    no

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,15 @@
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i = 0;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
