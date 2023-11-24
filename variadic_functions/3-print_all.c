39228c6683d6e728ffcc523c2af6e59d8cc64059
root <huseynisrafilov17@gmail.com>
2023-11-24 11:23:33 -0800

    no

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -6,14 +6,16 @@ void print_all(const char * const format, ...)
 {
 	va_list ap;
 	char *s;
-	int i = 0;
+	int i = 0, put_comma = 0;
 
 	va_start(ap, format);
 	while (*(format + i) != '\0')
 	{
+		put_comma = 0
 		switch(*(format + i))
 		{
 			case 's':
+				put_comma = 1;
 				s = va_arg(ap, char *);
 				if (s != NULL)
 				{
@@ -23,18 +25,21 @@ void print_all(const char * const format, ...)
 				printf("(nil)");
 				break;
 			case 'c':
+				put_comma = 1;
 				printf("%c", va_arg(ap, int));
 				break;
 			case 'i':
+				put_comma = 1;
 				printf("%d", va_arg(ap, int));
 				break;
 			case 'f':
+				put_comma = 1;
 				printf("%f", va_arg(ap, double));
 				break;
 			default:
 				break;
 		}
-			if (*(format + i + 1) != '\0')
+			if (*(format + i + 1) != '\0' && put_comma == 1)
 				printf(", ");
 			i++;
 	}
