61e89a653a439391945ac361d370d92e04626e4b
root <huseynisrafilov17@gmail.com>
2023-11-25 08:58:44 -0800

    no

--- function_pointers/100-main_opcodes.c
+++ function_pointers/100-main_opcodes.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_own_opcodes(int num_bytes) {
-    if (num_bytes < 0) {
-        fprintf(stderr, "Error\n");
-        exit(2);
-    }
+void print_own_opcodes(int num_bytes)
+{
+	unsigned char *opcodes;
+	int i;
 
-    // Dummy code to get the address of the next instruction
-    asm("call next\nnext: pop %%rax" : : : "rax");
-
-    // Print the opcodes
-    unsigned char *opcodes = (unsigned char *)print_own_opcodes;
-    for (int i = 0; i < num_bytes; ++i) {
-        printf("%02x", opcodes[i]);
-        if (i < num_bytes - 1) {
-            printf(" ");
-        }
-    }
-    printf("\n");
+	if (num_bytes < 0) 
+	{
+		fprintf(stderr, "Error\n");
+		exit(2);
+	}
+	asm("call next\nnext: pop %%rax" : : : "rax");
+	opcodes = (unsigned char *)print_own_opcodes;
+	for (i = 0; i < num_bytes; ++i) {
+		printf("%02x", opcodes[i]);
+		if (i < num_bytes - 1)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Error\n");
-        return 1;
-    }
+int main(int argc, char *argv[])
+{
+	int num_bytes;
 
-    int num_bytes = atoi(argv[1]);
-    print_own_opcodes(num_bytes);
-
-    return 0;
+	if (argc != 2) {
+		fprintf(stderr, "Error\n");
+		return 1;
+	}
+	num_bytes = atoi(argv[1]);
+	print_own_opcodes(num_bytes);
+	return (0);
 }
