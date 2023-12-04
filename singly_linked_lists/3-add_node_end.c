b4c5500cafcc13d1bf1fac03a6b44a7ac3417255
root <huseynisrafilov17@gmail.com>
2023-12-04 05:11:29 -0800

    3rd task

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <string.h>
+/**
+ * add_node_end - adds node to the end of the list.
+ * @head: head of the list/
+ * @str: string.
+ * Return: pointer of the added element.
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new, *temp;
@@ -23,5 +29,5 @@ list_t *add_node_end(list_t **head, const char *str)
 		temp->next = new;
 		new->next = NULL;
 	}
-	return(new);
+	return (new);
 }
