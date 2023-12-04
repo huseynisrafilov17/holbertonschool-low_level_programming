f0e0cb7a53ff9aeaa66f397faa3ccce549496e9e
root <huseynisrafilov17@gmail.com>
2023-12-04 05:32:09 -0800

    advanced task

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
+
 void printbefore(void) __attribute__ ((constructor));
 /**
  * printbefore - prints something before main function is executed.
  */
 void printbefore(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
