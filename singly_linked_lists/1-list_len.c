5d6ce9f35e13da492f94c645658a50d8b57aef86
root <huseynisrafilov17@gmail.com>
2023-12-04 03:30:16 -0800

    1st task done

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * print_list - prints everything inside linked list.
+ * list_len - returns the number of elements inside a linked list.
  * @h: list.
  * Return: node count.
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	unsigned int count = 0;
 
