7b082c64ef8d4fe7186017360759ae259b1737da
root <huseynisrafilov17@gmail.com>
2023-12-04 04:27:55 -0800

    2nd done

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * add_node.
+ * add_node - adds node to the linked list.
+ * @head: head of linked list.
+ * @str: string.
+ * Return: pointer to new node of the linked list.
  */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -12,7 +15,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	new->str = strdup(str);
 	new->len = strlen(str);
-	new->next = head;
-	head = new;
+	new->next = *head;
+	*head = new;
 	return (new);
 }
