0e146e7077b2105d49bb76e81d9d0e9254eedd6a
root <huseynisrafilov17@gmail.com>
2023-12-04 05:02:41 -0800

    2nd task

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *temp;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	temp = *head;
+	while (*head != NULL && temp->next != NULL)
+		temp = temp->next;
+	new->str = strdup(str);
+	new->len = strlen(str);
+	if (head == NULL)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+		temp->next = new;
+		new->next = NULL;
+	}
+	return(new);
+}
