eb71905387ed3948b9ec456fa8e9368865558676
root <huseynisrafilov17@gmail.com>
2023-12-04 03:16:39 -0800

    Fixes

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,9 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stddef.h>
-
-size_t print_list(const list_t *h);
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -18,5 +16,7 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
 
 #endif
