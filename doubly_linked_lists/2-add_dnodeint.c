7e3268d6d866b662520c7ccb7a997f88ef4f916a
root <huseynisrafilov17@gmail.com>
2023-12-06 03:01:28 -0800

    3rd task

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -8,10 +8,10 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_h *new;
-	new = malloc(sizeof(dlistint_h));
+	dlistint_t *new;
+	new = malloc(sizeof(dlistint_t));
 	if (*head != NULL)
-		(*head)->prev = new;
+		(**head).prev = new;
 	new->n = n;
 	new->next = *head;
 	new->prev = NULL;
