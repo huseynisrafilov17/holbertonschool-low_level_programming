98a345e8632fa3cfb0a0398808d900db26808ac3
root <huseynisrafilov17@gmail.com>
2023-12-06 07:14:51 -0800

    fixed

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *temp;
 
 	temp = head;
-	while (count <= index && temp != NULL)
+	while (count < index && temp != NULL)
 	{
 		temp = temp->next;
 		count++;
