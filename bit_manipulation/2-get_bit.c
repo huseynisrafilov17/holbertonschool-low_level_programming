6e3fc17005251eca37189007c8107a6e29386471
root <huseynisrafilov17@gmail.com>
2023-12-09 02:50:21 -0800

    2nd task

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * get_bit - gets bit at the given index.
+ * @n: number.
+ * @index: index.
+ * Return: bit or -1.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int num, size = 0, j, size_ulong = sizeof(n) * 8;
+	unsigned long int i = 1;
+
+	if (n == 0 && index == 0)
+		return (0);
+	else if (n == 0 && index != 0)
+		return (-1);
+	while (n >= i && size_ulong > size)
+	{
+		size++;
+		i *= 2;
+	}
+	if (index <= size - 1)
+		return ((n >> index) & 1);
+	else
+		return (-1);
+}
