c6bb413d68675fe8db1b13a367256f1618c5450a
root <huseynisrafilov17@gmail.com>
2023-12-13 06:07:11 -0800

    1st

---
+++ file_io/1-create_file.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+int create_file(const char *filename, char *text_content)
+{
+	int fd, n, len;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	len = strlen(text_content);
+	n = write(fd, text_content, len);
+	if (fd == -1 || n == -1)
+		return (-1);
+	return (0);
+}
