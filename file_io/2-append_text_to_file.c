214958ce1532f1f49a45b99d64f93a8c572bc5f1
root <huseynisrafilov17@gmail.com>
2023-12-15 03:23:19 -0800

    2nd task

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, n, len;
 
-	fd = open(filename, O_WR | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (text_content == NULL)
 		text_content = "";
 	len = strlen(text_content);
