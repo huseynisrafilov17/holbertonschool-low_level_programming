4590296c2dab54779e55e0478de0a7a01176d9e7
root <huseynisrafilov17@gmail.com>
2023-12-16 04:39:15 -0800

    partial fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -19,10 +19,7 @@ void needed(const char *filename, int fd, char *text_content)
 	while (i < len)
 	{
 		if (text_content[i] != '\0' && n != -1)
-		{
-			n = write(fd, text_content, 1);
-			text_content++;
-		}
+			n = write(fd, text_content + i, 1);
 		else if (n == -1)
 			break;
 		i++;
