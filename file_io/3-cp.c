a4da5867b534dff4d5ec563005b23180ac43528d
root <huseynisrafilov17@gmail.com>
2023-12-16 03:50:04 -0800

    fixes

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,25 +9,18 @@
  * @filename: file name.
  * @text_content: text content.
  */
-void needed(const char *filename, char *text_content)
+void needed(const char *filename, int fd, char *text_content)
 {
-	int fd, n, len, close_val;
+	int n, len, close_val;
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (text_content == NULL)
 		text_content = "";
 	len = strlen(text_content);
 	n = write(fd, text_content, len);
-	close_val = close(fd);
-	if (fd == -1 || n == -1)
+	if (n == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
 		exit(99);
-	}
-	if (close_val == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
 	}
 }
 /**
@@ -38,27 +31,37 @@ void needed(const char *filename, char *text_content)
  */
 int main(int argc, char *argv[])
 {
-	int fd, close_val, re;
+	int fd, fd1, close_val, close_val1, re;
 	char buff[1024];
 
 	if (argc != 3)
 		exit(97);
 	fd = open(argv[1], O_RDONLY);
+	fd1 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	re = read(fd, buff, 1024);
 	if (fd == -1 || re == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	while (re != -1)
+	if (fd1 == -1)
 	{
-		needed(argv[2], buff);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while (re != 0)
+	{
+		needed(argv[2], fd1, buff);
 		re = read(fd, buff, 1024);
 	}
 	close_val = close(fd);
-	if (close_val == -1)
+	close_val1 = close(fd1);
+	if (close_val == -1 || close_val1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		if (close_val1 == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		else
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 	return (1);
