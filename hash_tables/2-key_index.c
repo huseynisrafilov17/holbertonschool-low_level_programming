45913007b441dd4334d3a3fa4d12ef88d76f912a
root <huseynisrafilov17@gmail.com>
2023-12-17 01:30:28 -0800

    2nd

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+/**
+ * key_index - helps to find index for a key.
+ * @size: size of hash table.
+ * @key: key.
+ * Return: index.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash, index;
+
+	hash = hash_djb2((unsigned char *)key);
+	index = hash % index;
+	return (index);
+}
