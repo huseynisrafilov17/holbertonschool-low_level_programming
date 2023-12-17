a7a9373454c7bcc2cf37de5081873352915176ab
root <huseynisrafilov17@gmail.com>
2023-12-17 02:22:15 -0800

    fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -9,14 +9,13 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	int index, size;
+	int index;
 	hash_node_t **array, *new_node, *current_node, *check_node;
 
 	if (ht == NULL)
 		return (0);
-	size = ht->size;
 	array = ht->array;
-	index = key_index((unsigned char *)key, size);
+	index = key_index((unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
