761ae8954004e9e052fc45e9b50f11d5f4af18c4
root <huseynisrafilov17@gmail.com>
2023-12-17 02:12:33 -0800

    fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,24 +10,29 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int index, size = ht->size;
-	hash_node_t **array = ht->array, *new_node, *current_node;
+	hash_node_t **array = ht->array, *new_node, *current_node, check_node;
 
 	index = key_index((unsigned char *)key, size);
 	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	if (new_node == NULL || ht == NULL)
 		return (0);
 	new_node->key = (char *)key;
 	new_node->value = (char *)value;
 	if (!(array[index]))
-	{
-		array[index] = new_node;
 		new_node->next = NULL;
-	}
 	else
 	{
+		check_node = array[index];
+		while (check_node->next != NULL)
+		{
+			if (check_node->key == (char *)key)
+				check_node->value = (char *)value;
+			check_node = check_node->next;
+		}
 		current_node = array[index];
 		new_node->next = current_node;
 		array[index] = new_node;
 	}
+	array[index] = new_node;
 	return (1);
 }
