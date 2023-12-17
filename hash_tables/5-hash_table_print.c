9516d5330df33100d480105c5e60b32ac2677e9c
root <huseynisrafilov17@gmail.com>
2023-12-17 03:29:31 -0800

    fix

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -22,7 +22,7 @@ void hash_table_print(const hash_table_t *ht)
 		current_node = array[i];
 		while (current_node != NULL)
 		{
-			printed = 1
+			printed = 1;
 			printf("'%s': '%s'", current_node->key, current_node->value);
 			if (current_node->next != NULL)
 				printf(", ");
