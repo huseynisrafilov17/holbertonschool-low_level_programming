3193064ba6c2140750e22da5094205b1d9d5d44f
root <huseynisrafilov17@gmail.com>
2023-12-17 01:31:32 -0800

    major mistake

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -10,6 +10,6 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	unsigned long int hash, index;
 
 	hash = hash_djb2((unsigned char *)key);
-	index = hash % index;
+	index = hash % size;
 	return (index);
 }
