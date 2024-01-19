74149257693869ad4574c849ba2935955adf6f8a
root <huseynisrafilov17@gmail.com>
2024-01-19 05:26:57 -0800

    betty fix

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,6 +1,12 @@
 #include "search_algos.h"
 #include <stdio.h>
-
+/**
+ * binary_search - performs binary search on array.
+ * @array: array.
+ * @size: size of array.
+ * @value: value we are looking for.
+ * Return: index or 0.
+ */
 int binary_search(int *array, size_t size, int value)
 {
 	unsigned int i, m, l = 0, r = size - 1;
@@ -18,10 +24,10 @@ int binary_search(int *array, size_t size, int value)
 		m = (l + r) / 2;
 		if (array[m] > value)
 			r = m - 1;
-		else if (array[m] == value)
-			return (m);
-		else
+		else if (array[m] < value)
 			l = m + 1;
+		else
+			return (m);
 	}
 	return (-1);
 }
